de0b299edc41b48ade41d8eaf6969f272fff584a
PhamVuHaiDuong <77629198+PhamVuHaiDuong@users.noreply.github.com>
2023-05-11 21:28:36 +0700

    Add project files.

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Expression.Interactivity.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Test.Model;
+
+namespace Test.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public bool IsLogin { get; set; }
+        private string _Username;
+        public string UserName { get => _Username; set { _Username = value; OnPropertyChanged(); } }
+        private string _Password;
+        public string Password { get => _Password; set { _Password = value; OnPropertyChanged(); } }
+        public ICommand LoginCommand { get; set; }
+        public ICommand CloseCommand { get; set; }
+        public ICommand PasswordChangedComman { get; set; }
+        public LoginViewModel()
+        {
+            IsLogin = false;
+            Password = "";
+            UserName = "";
+            LoginCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { Login(p); });
+            CloseCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { p.Close(); });
+            PasswordChangedComman = new RelayCommand<PasswordBox>((p) => { return true; }, (p) => { Password = p.Password; });
+        }
+        void Login(Window p)
+        {
+            if (p == null)
+                return;
+            var accCount = DataProvider.Ins.DB.Users.Where(x => x.UserName == UserName && x.Password == Password).Count();
+            if (accCount > 0)
+            {
+                IsLogin = true;
+
+                p.Close();
+            }
+            else
+            {
+                IsLogin = false;
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu sai");
+            }
+        }
+    }
+}
